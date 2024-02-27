63f7294312da411fd4169385ee9a72fa71c83bf4
Mohamed Nawas <nawas@swivelgroup.com.au>
2024-02-27 21:31:01 +0530

    minor UI enhancement

--- client/src/pages/Signin/ui.tsx
+++ client/src/pages/Signin/ui.tsx
@@ -17,7 +17,7 @@ const SigninUI = (props: SigninUIProps) => {
                     <div>
                         <label htmlFor="email" className="sr-only">Email</label>
 
-                        <div className="relative" style={{borderWidth: '2px', borderColor: 'black'}}>
+                        <div className="relative">
                             <input
                                 id="email"
                                 type="email"
@@ -27,7 +27,7 @@ const SigninUI = (props: SigninUIProps) => {
                                 onChange={props.handleEmailChange}
                             />
 
-                            <span className="absolute inset-y-0 inset-x-200 grid place-content-center px-4">
+                            <span className="absolute inset-y-0 right-0 w-1/10 grid place-content-center px-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-4 w-4 text-gray-400"
@@ -59,7 +59,7 @@ const SigninUI = (props: SigninUIProps) => {
                                 onChange={props.handlePwdChange}
                             />
 
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                            <span className="absolute inset-y-0 right-0 w-1/10 grid place-content-center px-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-4 w-4 text-gray-400"
--- client/src/pages/Signup/ui.tsx
+++ client/src/pages/Signup/ui.tsx
@@ -27,7 +27,7 @@ const SignupUI = (props: SignupUIProps) => {
                                 onChange={props.handleNameChange}
                             />
 
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                            <span className="absolute inset-y-0 right-0 w-1/10 grid place-content-center px-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-4 w-4 text-gray-400"
@@ -39,7 +39,7 @@ const SignupUI = (props: SignupUIProps) => {
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
                                         strokeWidth="2"
-                                        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                                        d="M12 2c2.761 0 5 2.239 5 5s-2.239 5-5 5-5-2.239-5-5 2.239-5 5-5zM12 12c3.309 0 6 2.691 6 6v2H6v-2c0-3.309 2.691-6 6-6z"
                                     />
                                 </svg>
                             </span>
@@ -59,7 +59,7 @@ const SignupUI = (props: SignupUIProps) => {
                                 onChange={props.handleEmailChange}
                             />
 
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                            <span className="absolute inset-y-0 right-0 w-1/10 grid place-content-center px-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-4 w-4 text-gray-400"
@@ -91,7 +91,7 @@ const SignupUI = (props: SignupUIProps) => {
                                 onChange={props.handlePwdChange}
                             />
 
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                            <span className="absolute inset-y-0 right-0 w-1/10 grid place-content-center px-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-4 w-4 text-gray-400"
@@ -129,7 +129,7 @@ const SignupUI = (props: SignupUIProps) => {
                                 onChange={props.handleRepeatPwdChange}
                             />
 
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                            <span className="absolute inset-y-0 right-0 w-1/10 grid place-content-center px-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-4 w-4 text-gray-400"
