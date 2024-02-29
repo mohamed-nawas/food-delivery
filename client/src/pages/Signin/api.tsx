6c840be1198d6de146d88586878b4ffda2028249
mohamed-nawas <mgmnawas@gmail.com>
2024-02-29 11:03:05 +0530

    added fe api test for signin api

--- client/src/pages/Signin/api.tsx
+++ client/src/pages/Signin/api.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { AuthApiErrorResponse, AuthApiSuccessResponse } from '../../types/authTypes';
 
 const url = (email: string, pwd: string) => `http://localhost:8080/food-delivery/api/v1/auth/signin?email=${email}&pwd=${pwd}`;
@@ -6,12 +6,11 @@ const url = (email: string, pwd: string) => `http://localhost:8080/food-delivery
 export const signin = (email: string, pwd: string) => {
     return new Promise<AuthApiSuccessResponse>(async (res, rej) => {
         try {
-            const response = await axios.post(url(email, pwd), { email, pwd }, { headers: { "Content-Type": "application/json" } });
+            const response = await axios.post(url(email, pwd), { headers: { "Content-Type": "application/json" } });
             res(response as AuthApiSuccessResponse);
         } catch (e) {
-            console.log(`e: ${e}`)
-            if (e.response) {
-                rej(e.response as AuthApiErrorResponse)
+            if ((e as AxiosError).response) {
+                rej((e as AxiosError).response as AuthApiErrorResponse)
             } else {
                 rej({data: {message: "Something went wrong, please try again"}})
             }
