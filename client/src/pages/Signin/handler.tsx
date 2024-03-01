02554a24a13fa1b5dc75f556d0f4fcac3f070169
mohamed-nawas <mgmnawas@gmail.com>
2024-03-01 10:52:34 +0530

    fixed ts resolve issues

--- client/src/pages/Signin/handler.tsx
+++ client/src/pages/Signin/handler.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import * as apis from './api';
 import { useAppDispatch } from '../../redux/hooks';
 import { authActions} from '../../redux/auth/authSlice';
--- client/src/pages/Signin/index.tsx
+++ client/src/pages/Signin/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import useSignin from "./handler";
 import SigninUI from "./ui";
 
