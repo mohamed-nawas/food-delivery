e8f0bf3c8adf225823756eb67181679a42eecd06
mohamed-nawas <mgmnawas@gmail.com>
2024-03-29 20:03:47 +0530

    setup basic for main area of admin dashboard

---
+++ client/src/components/admin-dashboard/Main/ui.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface AdminDashboardMainUIProps {}
+
+const AdminDashboardMainUI = (props: AdminDashboardMainUIProps) => {
+  return (
+    <div className='flex-1 bg-gray-100 p-5'>right aside content</div>
+  );
+};
+
+export default AdminDashboardMainUI;
--- client/src/pages/AdminDashboard/ui.tsx
+++ client/src/pages/AdminDashboard/ui.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import AdminDashboardHeader from '../../components/admin-dashboard/Header';
 import AdminDashboardMenu from '../../components/admin-dashboard/Menu';
+import AdminDashboardMain from '../../components/admin-dashboard/Main';
 
 interface AdminDashboardUIProps {}
 
 const AdminDashboardUI = (props: AdminDashboardUIProps) => {
-    return (
-        <>
-            <AdminDashboardHeader />
-            <AdminDashboardMenu />
-        </>
-    )
-}
+  return (
+    <>
+      <AdminDashboardHeader />
+      <div className='flex'>
+        <AdminDashboardMenu />
+        <AdminDashboardMain />
+      </div>
+    </>
+  );
+};
 
 export default AdminDashboardUI;
