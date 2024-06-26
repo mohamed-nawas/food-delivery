edc74d40a6bd6c292a3f3e45a792e4ac8955c1d2
mohamed-nawas <mgmnawas@gmail.com>
2024-06-26 11:21:21 +0530

    aside menu toggler and responsive behavior

--- client/src/components/admin-dashboard/Aside/ui.tsx
+++ client/src/components/admin-dashboard/Aside/ui.tsx
@@ -7,12 +7,20 @@ interface AdminDashboardAsideUIProps {
 }
 
 const AdminDashboardAsideUI = (props: AdminDashboardAsideUIProps) => {
+  const [showAside, setShowAside] = React.useState(false);
+
+  const toggleAside = () => {
+    setShowAside(!showAside);
+  }
+
   return (
-    <aside className='feed-me__admin-dashboard__aside'>
-      <div className='feed-me__admin-dashboard__aside__toggler'></div>
-      <AdminDashboardAsideHeader />
-      <AdminDashboardAsideMenu />
-      <AdminDashboardAsideFooter />
+    <aside className='feed-me__admin-dashboard__aside' style={{ width: showAside ? '350px' : '', position: showAside ? 'absolute' : 'relative', top: 0, left: 0, zIndex: 999 }}>
+      <button onClick={toggleAside} className='feed-me__admin-dashboard__aside__toggler' style={{ marginLeft: showAside ? '3rem' : 'auto' }}></button>
+      <div style={{ display: showAside ? 'block' : '' }}>
+        <AdminDashboardAsideHeader />
+        <AdminDashboardAsideMenu />
+        <AdminDashboardAsideFooter />
+      </div>
     </aside>
   );
 };
